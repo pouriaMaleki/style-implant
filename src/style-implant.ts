84c2974c51a677ad36499e2e456ae24147612da5
Ivo Ilić <ilicivo@gmail.com>
2021-08-19 22:57:50 -0700

    Matched the latest version of style-inject with tests and types

--- src/style-implant.ts
+++ src/style-implant.ts
@@ -1,3 +1,40 @@
-export const placeholder = (input: boolean): boolean => {
-  return !input;
+const styleImplant = (
+  css: string,
+  {
+    insertAt,
+  }: {
+    insertAt?: string | null;
+  } = {}
+): boolean => {
+  if (!css || typeof document === 'undefined') return false;
+
+  const head = document.head || document.getElementsByTagName('head')[0];
+  const style: HTMLStyleElementExtended = document.createElement('style');
+  style.type = 'text/css';
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+
+  return true;
 };
+
+interface HTMLStyleElementExtended extends HTMLStyleElement {
+  styleSheet?: {
+    cssText?: string;
+  };
+}
+
+export default styleImplant;
--- tests/style-implant.test.ts
+++ tests/style-implant.test.ts
@@ -1,13 +1,65 @@
 import { expect } from 'chai';
-import { placeholder } from '../src/style-implant';
+import * as fs from 'fs';
+import * as http from 'http';
+import * as puppeteer from 'puppeteer';
 
-describe('style-implant unit tests', (): void => {
-  it('input true', (): void => {
-    const result: boolean = placeholder(true);
-    expect(result).is.equal(false);
+let page: puppeteer.Page;
+let server: http.Server;
+const url = `http://localhost:8080/test.html`;
+
+const addImplant = async (css?: string, options?: unknown) => {
+  const implantInputs = options ? `"${css}",${JSON.stringify(options)}` : `"${css}"`;
+  await page.evaluate(implantInputs => {
+    const head = document.head;
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.innerHTML = `import styleImplant from "/style-implant.js"; styleImplant(${implantInputs})`;
+    head.appendChild(script);
+    return;
+  }, implantInputs);
+};
+
+const getTestColor = async () => {
+  const color = await page.evaluate(() => {
+    const testElement = document.getElementById('test');
+    return window.getComputedStyle(testElement, null).getPropertyValue('color');
   });
-  it('input false', (): void => {
-    const result: boolean = placeholder(false);
-    expect(result).is.equal(true);
+  return color;
+};
+
+describe('style-implant unit tests', async (): void => {
+  before(async function () {
+    server = http.createServer((req, res) => {
+      if (req.url === '/style-implant.js') {
+        res.writeHead(200, { 'content-type': 'text/javascript' });
+        fs.createReadStream('./dist/style-implant.js').pipe(res);
+      } else {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        fs.createReadStream('./tests/test.html').pipe(res);
+      }
+    });
+    server.listen(8080);
+    global.expect = expect;
+    global.browser = await puppeteer.launch({ headless: true });
+    page = await browser.newPage();
+  });
+  it('page loaded', async (): void => {
+    await Promise.all([page.goto(url, { timeout: 0 }), page.waitForNavigation({ timeout: 0 })]);
+    expect(await page.title()).to.eql('style-implant');
+  });
+  it('undefined css', async (): void => {
+    await addImplant(null, {});
+    const testElementColor = await getTestColor();
+    expect(testElementColor).is.equal('rgb(0, 0, 0)');
+  });
+  it('no options', async (): void => {
+    await addImplant('.test{color: red;}', {});
+    const testElementColor = await getTestColor();
+    expect(testElementColor).is.equal('rgb(255, 0, 0)');
+  });
+  after(async function () {
+    await page.close();
+    await browser.close();
+    await server.close();
   });
 });
